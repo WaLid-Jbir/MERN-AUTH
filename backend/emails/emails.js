158030b4fec280ecdd31fd6ca85dad1eba99550b
WaLid-Jbir <anawalid737@gmail.com>
2025-02-11 19:27:19 +0100

    Creating the forgot password function endpoint

--- backend/emails/emails.js
+++ backend/emails/emails.js
@@ -1,5 +1,5 @@
 import { mailtrapClient, sender } from "../emails/mailtrap.config.js";
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { VERIFICATION_EMAIL_TEMPLATE, PASSWORD_RESET_REQUEST_TEMPLATE } from "./emailTemplates.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
     const recepient = [{email}];
@@ -18,4 +18,24 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         console.error(`Error sending verification email: ${error}`);
         throw new Error(`Error sending verification email: ${error}`);
     }
-}
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+    const recepient = [{email}];
+    try {
+        
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recepient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+            category: "Password Reset"
+        });
+
+        console.log("Reset email sent successfully", response);
+
+    } catch (error) {
+        console.error(`Error sending reset password email: ${error}`);
+        throw new Error(`Error sending reset password email: ${error}`);
+    }
+};
