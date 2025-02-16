3ab056337deedd844a9bd3f4fb66a0aa81810590
WaLid-Jbir <anawalid737@gmail.com>
2025-02-16 03:02:45 +0100

    Creating the login and signup pages (UI)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom';
+import FloatingShape from './components/FloatingShape';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <>
-      <h1 className='text-red-500 text-lg'>Hello world!</h1>
-    </>
-  )
+    <div className='min-h-screen bg-gradient-to-br from-blue-900 via-blue-800 to-blue-700 flex items-center justify-center relative overflow-hidden'>
+      <FloatingShape color='bg-blue-400' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+      <FloatingShape color='bg-blue-300' size='w-48 h-48' top='70%' left='80%' delay={5} />
+      <FloatingShape color='bg-blue-200' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+      <FloatingShape color='bg-blue-100' size='w-24 h-24' top='20%' left='50%' delay={2} />
+
+      <Routes>
+        {/* Your routes here */}
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
